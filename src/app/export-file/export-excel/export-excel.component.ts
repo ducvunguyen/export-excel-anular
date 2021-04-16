e4b998a07681f7ee4551e05b5a75eba22e9075f6
ducvunguyen <vu97kt@gmail.com>
2021-04-16 15:40:12 +0700

    export excel done

--- src/app/export-file/export-excel/export-excel.component.ts
+++ src/app/export-file/export-excel/export-excel.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as logoFile from '../../carlogo';
 import {Workbook} from 'exceljs';
 import * as fs from 'file-saver';
-
+import {POSITION_CELLS} from '../constants';
+import {element} from 'protractor';
 @Component({
   selector: 'app-export-excel',
   templateUrl: './export-excel.component.html',
@@ -10,7 +11,10 @@ import * as fs from 'file-saver';
 })
 export class ExportExcelComponent implements OnInit {
   dataSource = [
-
+    [1, 'Khối 1', 3, 4, 4, 4, 4, 4, 4, 4, 10, 10],
+    [2, 'Khối 2', 3, 4, 4, 4, 4, 4, 4, 4, 10, 10],
+    [3, 'Khối 3', 3, 4, 4, 4, 4, 4, 4, 4, 10, 10],
+    [4, 'Khối 4', 3, 4, 4, 4, 4, 4, 4, 4, 10, 10],
   ]
 
   constructor() {
@@ -21,100 +25,107 @@ export class ExportExcelComponent implements OnInit {
   }
 
   clickExport(){
-    let title = 'Thống kê bài kiểm tra';
-    let header = [];
-    let data = [];
-    let nameFile = '';
-    let colSpan = [];
-
-
-
-    this.generateExcel(title, header, data, nameFile, colSpan);
+    this.generateExcel();
   }
-  async generateExcel(title: string, header: any[], data: any, nameFile: string, colSpan: number[]) {
-    // const ExcelJS = await import('exceljs');
-    // console.log(ExcelJS);
-    // const Workbook: any = {};
+  async generateExcel() {
     let titleName = 'Trường THPT OMT PHÒNG GD Đống Đa';
-    let titleSchool = 'Thống kê bài kiểm tra';
-    let schoolYear = 'Năm học: 2020 - 2021';
-    let subject = 'Lớp: Toán 6A1';
-    let teacher = 'Giáo viên: Nguyễn Văn  A, Trần Thị B';
+    let labelSubject = ['' ,'' ,'Toán', 'Văn', 'Anh ', 'Sinh', 'Sử', 'Địa', 'Lý', 'Hóa', 'GDCD', 'Kỹ năng sống'];
+    let borderGeneral = {
+      top: {style:'thin', color: {argb:'000000'}},
+      left: {style:'thin', color: {argb:'000000'}},
+      bottom: {style:'thin', color: {argb:'000000'}},
+      right: {style:'thin', color: {argb:'000000'}}
+    };
 
-    // Create workbook and worksheet
-    const workbook = new Workbook();
-    const worksheet = workbook.addWorksheet('Car Data');
+    let fontGeneralHeader = {
+      name: 'Calibri',
+      color: { argb: '000000' },
+      family: 4,
+      size: 11,
+      bold: true,
+    };
 
-
-    // Add Row and formatting
-    const titleRow = worksheet.addRow([titleName, ,titleSchool]);
-    titleRow.font = {name: 'Comic Sans MS', family: 4, size: 16, underline: 'double', bold: true};
-    worksheet.mergeCells('A1:B1:A2');
-    worksheet.mergeCells('C1:L1');
-    worksheet.getColumn(2).width = 30;
-    worksheet.addRow([]);
-    // Add Header Row
-    const headerRow = worksheet.addRow(header);
-
-    // Cell Style : Fill and Border
-    headerRow.eachCell((cell, number) => {
-      cell.fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: {argb: 'FF8038'},
-        bgColor:{argb:'4C0B5F'}
-      };
-      cell.border = {top: {style: 'dotted'}, left: {style: 'dotted'}, bottom: {style: 'dotted'}, right: {style: 'dotted'}};
-    });
-
-    data.forEach(d => {
-        const row = worksheet.addRow(d);
-        const qty = row.getCell(6); //chon colums de xu ly
-        let color = 'FF99FF99';
-        if (+qty.value < 500) {
-          color = '3104B4';
-        }
-
-        qty.fill = {
-          type: 'pattern',
-          pattern: 'solid',
-          fgColor: {argb: color}
-        };
-      }
-    );
-
-    worksheet.getColumn(1).width = 30;//tang kich thuoc cua cot
-    worksheet.getColumn(3).width = 30;
-    worksheet.getColumn(4).width = 30;
-    worksheet.addRow([]);
-
-    let customTest = [...new Set(colSpan)];
-    let dataTest = [];
-    let row = 13;
-    customTest.forEach(item => dataTest.push(colSpan.filter(col => col === item))) ;
-    dataTest.forEach(item =>{
-      worksheet.mergeCells(`A${row}:A${row + item.length - 1}`);
-      row += item.length;
-    });
-
-
-    // Footer Row
-    const footerRow = worksheet.addRow(['This is system generated excel sheet.']);
-    footerRow.getCell(1).fill = {
+    let fillBgHeader = {
       type: 'pattern',
       pattern: 'solid',
-      fgColor: {argb: 'FFCCFFE5'}
+      fgColor: {argb: '2EFE9A'},
+      bgColor:{argb:'4C0B5F'}
     };
-    footerRow.getCell(1).border = {top: {style: 'thin'}, left: {style: 'thin'}, bottom: {style: 'thin'}, right: {style: 'thin'}};
 
-    // Merge Cells
-    worksheet.mergeCells(`A${footerRow.number}:F${footerRow.number}`);
-    // worksheet.mergeCells('G1:H1');//merge 2 o voi nhau
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('Thống kê lớp học');
 
-    // Generate Excel File with given name
+    // Add Row and formatting
+    const titleRow = worksheet.addRow([titleName]);
+    titleRow.font = {name: 'Calibri', family: 4, size: 11, underline: 'none', bold: false};
+    let verticalCenter = { vertical: 'middle', horizontal: 'center',  wrapText: true };
+    worksheet.mergeCells('A1:B2');
+    worksheet.mergeCells(`C1:${POSITION_CELLS[labelSubject.length-1]}1`);
+    worksheet.mergeCells(`C2:${POSITION_CELLS[labelSubject.length-1]}2`);
+
+    worksheet.getCell('C1').value = 'Thống kê lớp học'.toUpperCase();
+    worksheet.getCell('C1').alignment = verticalCenter;
+    worksheet.getCell('C1').font = {name: 'Calibri', size: 20, bold: true };
+    worksheet.getCell('A1').alignment = verticalCenter;
+
+    worksheet.getCell('C2').value = 'Năm học: 2020 - 2021';
+    worksheet.getCell('C2').alignment = verticalCenter;
+    worksheet.getRow(1).height = 30;
+
+    labelSubject.forEach((item, index) => index == 0 ? null : worksheet.getColumn(index+1).width = 20);
+
+    worksheet.getCell('A4').value = 'STT';
+    worksheet.getCell('B4').value = 'Khối';
+    worksheet.getCell('C4').value = 'Lớp';
+
+    worksheet.getCell('A4').fill = fillBgHeader;
+    worksheet.getCell('A4').font = fontGeneralHeader;
+    worksheet.getCell('A4').border = borderGeneral;
+
+    worksheet.getCell('B4').fill = fillBgHeader;
+    worksheet.getCell('B4').font = fontGeneralHeader;
+
+    worksheet.getCell('B4').border = borderGeneral;
+    worksheet.getCell('B4').font = fontGeneralHeader;
+
+    worksheet.getCell('C4').fill = fillBgHeader;
+    worksheet.getCell('C4').font = fontGeneralHeader;
+    worksheet.getCell('C4').border = borderGeneral
+
+    const headerRowSubject = worksheet.addRow(labelSubject);
+    headerRowSubject.eachCell((cell, number) => {
+      cell.fill = fillBgHeader;
+      cell.border = borderGeneral;
+      cell.alignment = verticalCenter;
+      cell.font = fontGeneralHeader;
+    });
+
+    worksheet.mergeCells('A4:A5');
+    worksheet.mergeCells('B4:B5');
+    worksheet.mergeCells(`C4:${POSITION_CELLS[labelSubject.length-1]}4`);
+    worksheet.getCell('A4').alignment = verticalCenter;
+    worksheet.getCell('B4').alignment = verticalCenter;
+    worksheet.getCell('C4').alignment = verticalCenter;
+
+    this.dataSource.forEach(element => {
+      let addRow = worksheet.addRow(element);
+      addRow.eachCell((cell, index) => {
+        index !== 2 ? cell.alignment = verticalCenter : null;
+        cell.border = {
+          top: {style:'thin', color: {argb:'000000'}},
+          left: {style:'thin', color: {argb:'000000'}},
+          bottom: {style:'thin', color: {argb:'000000'}},
+          right: {style:'thin', color: {argb:'000000'}}
+        };
+      })
+    });
+
+    worksheet.addRow([]);
+    worksheet.addRow(['Lưu ý: Danh sách lớp học được triển khai trong năm học bao gồm các lớp đang dạy, đã kết thúc; không bao gồm các lớp học đã hủy, chưa triển khai']);
+
     workbook.xlsx.writeBuffer().then((data: any) => {
       const blob = new Blob([data], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
-      fs.saveAs(blob, nameFile+'.xlsx');
+      fs.saveAs(blob, 'thong_ke_lop_hoc.xlsx');
     });
 
   }
